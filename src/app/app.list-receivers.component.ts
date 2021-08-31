d120db87baa66679b4838a2900b7408a348fb5b8
BryZe NtZa <ntbryze@gmail.com>
2021-08-31 12:39:25 +0100

    07/06/2021 feedback

--- src/app/app.list-receivers.component.ts
+++ src/app/app.list-receivers.component.ts
@@ -18,18 +18,18 @@ export interface ReceiverInterface {
 }
 
 const ELEMENT_DATA: ReceiverInterface[] = [
-  {position: 1, numero:'00001', nom: 'Peeters', prenom: 'Brice', niss:'', email:'brice.peters@baotec.com', langue:'Français', dateact:'18/06/2021'},
-  {position: 2, numero:'00002', nom: 'Janssens', prenom: 'Joel', niss:'', email:'joel.janssens@doccle.com', langue:'Français', dateact:'18/06/2021'},
-  {position: 3, numero:'00003', nom: 'Maes', prenom: 'Christelle', niss:'', email:'chrsitelle.maes@doccle.com', langue:'Anglais', dateact:'18/06/2021'},
-  {position: 4, numero:'00004', nom: 'Jacobs', prenom: 'Berthold', niss:'', email:'brice.jacobs@doccle.com', langue:'Français', dateact:'18/06/2021'},
-  {position: 5, numero:'00005', nom: 'Mertens', prenom: 'Yvan', niss:'', email:'yvan.mertens@kps-it.com', langue:'Anglais', dateact:'18/06/2021'},
-  {position: 6, numero:'00006', nom: 'Willems', prenom: 'Guy', niss:'', email:'guy.willems@baotec.com', langue:'Français', dateact:'18/06/2021'},
-  {position: 7, numero:'00007', nom: 'Claes', prenom: 'Sévérin', niss:'', email:'severin.claes@kps-it.com', langue:'Français', dateact:'18/06/2021'},
-  {position: 8, numero:'00008', nom: 'Goossens', prenom: 'Joel', niss:'', email:'joel.goossens@doccle.com', langue:'Français', dateact:'18/06/2021'},
-  {position: 9, numero:'00009', nom: 'Wouters', prenom: 'Luis', niss:'', email:'wouters@kps-it.com', langue:'Français', dateact:'18/06/2021'},
-  {position: 10, numero:'00010', nom: 'De Smet', prenom: 'Karele', niss:'', email:'de.smet@gmail.com', langue:'Français', dateact:'18/06/2021'},
-  {position: 11, numero:'00011', nom: 'Leclercq', prenom: 'Judicael', niss:'', email:'leclercq@gmail.com', langue:'Français', dateact:'18/06/2021'},
-  {position: 12, numero:'00012', nom: 'Renard', prenom: 'Marthe', niss:'', email:'renard@yahoo.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 1, numero:'00001', nom: 'Peeters', prenom: 'Brice', niss:'85 07 30 033 28', email:'brice.peters@baotec.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 2, numero:'00002', nom: 'Janssens', prenom: 'Joel', niss:'17 07 30 033 84', email:'joel.janssens@doccle.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 3, numero:'00003', nom: 'Maes', prenom: 'Christelle', niss:'85 07 30 033 28', email:'chrsitelle.maes@doccle.com', langue:'Anglais', dateact:'18/06/2021'},
+  {position: 4, numero:'00004', nom: 'Jacobs', prenom: 'Berthold', niss:'17 07 30 033 84', email:'brice.jacobs@doccle.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 5, numero:'00005', nom: 'Mertens', prenom: 'Yvan', niss:'85 07 30 033 28', email:'yvan.mertens@kps-it.com', langue:'Anglais', dateact:'18/06/2021'},
+  {position: 6, numero:'00006', nom: 'Willems', prenom: 'Guy', niss:'85 07 30 033 28', email:'guy.willems@baotec.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 7, numero:'00007', nom: 'Claes', prenom: 'Sévérin', niss:'85 07 30 033 28', email:'severin.claes@kps-it.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 8, numero:'00008', nom: 'Goossens', prenom: 'Joel', niss:'85 07 30 033 28', email:'joel.goossens@doccle.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 9, numero:'00009', nom: 'Wouters', prenom: 'Luis', niss:'85 07 30 033 28', email:'wouters@kps-it.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 10, numero:'00010', nom: 'De Smet', prenom: 'Karele', niss:'85 07 30 033 28', email:'de.smet@gmail.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 11, numero:'00011', nom: 'Leclercq', prenom: 'Judicael', niss:'85 07 30 033 28', email:'leclercq@gmail.com', langue:'Français', dateact:'18/06/2021'},
+  {position: 12, numero:'00012', nom: 'Renard', prenom: 'Marthe', niss:'85 07 30 033 28', email:'renard@yahoo.com', langue:'Français', dateact:'18/06/2021'},
 ];
 
 @Component({
